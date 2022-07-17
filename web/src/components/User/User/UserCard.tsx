63a750b130a989af0aad5b0b083e1d6f976f86d9
Simas <simonas.berankis@telesoftas.com>
2022-07-17 12:41:46 +0300

    feat: styling all users page

--- web/src/components/User/User/UserCard.tsx
+++ web/src/components/User/User/UserCard.tsx
@@ -1,14 +1,28 @@
+import { Card, CardMedia, CardContent, Typography } from '@mui/material'
+
 import { User } from '../../../../types/graphql'
 
 export const UserCard = ({ user, onClick }: UserCartProps) => {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div onClick={() => onClick()}>
-      <img src={user.imageUrl} alt={'profilePicture'} width="300px" />
-      <br />
-      Username: {user.username}
-      <br />
-      Role: {user.role}
+      <Card sx={{ width: 300, height: 400, margin: 1 }}>
+        <CardMedia
+          sx={{ maxWidth: 400 }}
+          component="img"
+          height="300"
+          image={user.imageUrl}
+          alt="profilePicture"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Username: {user.username}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Role: {user.role}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
